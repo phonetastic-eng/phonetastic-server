be67f774952946170bdaa51fdd8def73b50f1bd3
Jordan Gaston <jordangaston1994@gmail.com>
2026-03-03 19:57:08 -0600

    Get test calls working

--- src/agent.ts
+++ src/agent.ts
@@ -6,7 +6,6 @@ import {
   voice,
 } from '@livekit/agents';
 import * as livekit from '@livekit/agents-plugin-livekit';
-import { ParticipantKind } from '@livekit/rtc-node';
 import * as silero from '@livekit/agents-plugin-silero';
 import { DBOSClient } from '@dbos-inc/dbos-sdk';
 import 'dotenv/config';
@@ -45,14 +44,33 @@ export default defineAgent({
       turnDetection: new livekit.turnDetector.MultilingualModel(),
     });
 
+    session.once(voice.AgentSessionEventTypes.Close, async (ev: voice.CloseEvent) => {
+      log().info('Session closed');
+      await livekitService.deleteRoom(roomName);
+      ctx.shutdown()
+      return;
+    });
+
+    session.on(voice.AgentSessionEventTypes.Error, async (ev: voice.ErrorEvent) => {
+      const error: any = ev.error;
+      if (error?.recoverable) {
+        log().error('Recoverable error', ev.error);
+      } else {
+        log().error('Unrecoverable error', ev.error);
+      }
+    });
+
     await session.start({ agent, room: ctx.room });
     await ctx.connect();
 
     log().info({ roomName }, 'Call connected');
 
-    if (!isTestCall(roomName)) {
+    const caller = await ctx.waitForParticipant();
+    if (isTestCall(roomName)) {
+      log().info({ caller }, 'Caller found');
+      await callService.onParticipantJoined(roomName);
+    } else {
       try {
-        const caller = await ctx.waitForParticipant();
         log().info({ caller }, 'Caller found');
         const from = caller.attributes['sip.phoneNumber'] ?? '';
         const to = caller.attributes['sip.trunkPhoneNumber'] ?? '';
@@ -62,16 +80,12 @@ export default defineAgent({
       } catch (err: any) {
         log().error({ err }, 'Failed to initialize inbound call');
         await session.generateReply({
-          instructions: `Say "${err.message ?? 'Something went wrong'}"`,
+          instructions: 'Inform the caller that something went wrong and to try again later.',
         }).waitForPlayout();
+        await sleep(2000);
         session.shutdown();
         await ctx.room.disconnect();
-        const sipParticipant = [...ctx.room.remoteParticipants.values()]
-          .find(p => p.kind === ParticipantKind.SIP);
-        if (sipParticipant) {
-          log().info({ identity: sipParticipant.identity }, 'Removing SIP participant');
-          await livekitService.removeParticipant(roomName, sipParticipant.identity);
-        }
+        await livekitService.removeParticipant(roomName, caller.identity);
         return;
       }
     }
@@ -82,3 +96,7 @@ export default defineAgent({
     });
   }
 });
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}

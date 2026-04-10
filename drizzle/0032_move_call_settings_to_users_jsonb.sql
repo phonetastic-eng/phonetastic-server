2fbba9a0484d131f02aba7b51164bb9300a341f8
Jordan Gaston <jordangaston1994@gmail.com>
2026-04-10 14:00:06 -0500

    Fix migration

--- drizzle/0032_move_call_settings_to_users_jsonb.sql
+++ drizzle/0032_move_call_settings_to_users_jsonb.sql
@@ -1,20 +1,25 @@
-ALTER TABLE "users" ADD COLUMN "call_settings" jsonb NOT NULL DEFAULT '{}';
+ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "call_settings" jsonb NOT NULL DEFAULT '{}';
 
-UPDATE "users"
-SET "call_settings" = (
-  SELECT jsonb_build_object(
-    'forwardedPhoneNumberId', cs.forwarded_phone_number_id,
-    'companyPhoneNumberId', cs.company_phone_number_id,
-    'isBotEnabled', cs.is_bot_enabled,
-    'ringsBeforeBotAnswer', cs.rings_before_bot_answer,
-    'answerCallsFrom', cs.answer_calls_from::text,
-    'sipDispatchRuleId', cs.sip_dispatch_rule_id
-  )
-  FROM "call_settings" cs
-  WHERE cs.user_id = "users".id
-)
-WHERE EXISTS (SELECT 1 FROM "call_settings" cs WHERE cs.user_id = "users".id);
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'call_settings') THEN
+    UPDATE "users"
+    SET "call_settings" = (
+      SELECT jsonb_build_object(
+        'forwardedPhoneNumberId', cs.forwarded_phone_number_id,
+        'companyPhoneNumberId', cs.company_phone_number_id,
+        'isBotEnabled', cs.is_bot_enabled,
+        'ringsBeforeBotAnswer', cs.rings_before_bot_answer,
+        'answerCallsFrom', cs.answer_calls_from::text,
+        'sipDispatchRuleId', cs.sip_dispatch_rule_id
+      )
+      FROM "call_settings" cs
+      WHERE cs.user_id = "users".id
+    )
+    WHERE EXISTS (SELECT 1 FROM "call_settings" cs WHERE cs.user_id = "users".id);
+  END IF;
+END $$;
 
-DROP TABLE "call_settings";
+DROP TABLE IF EXISTS "call_settings";
 
 DROP TYPE IF EXISTS "answer_calls_from";
